5ad87bd7e3bc6f98e8386b12c9479db79fccefe6
MarceloBezz <mdbezerra16@gmail.com>
2025-04-01 13:50:41 -0300

    Primeiro teste do AdocaoController

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,41 @@
+package br.com.alura.adopet.api.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import br.com.alura.adopet.api.service.AdocaoService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AdocaoService adocaoService;
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception{
+        //ARRAGE
+        String json = "{}";
+
+        //ACT
+        var response = mockMvc.perform(
+                MockMvcRequestBuilders
+                .post("/adocoes")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        Assertions.assertEquals(400, response);
+    }
+}
